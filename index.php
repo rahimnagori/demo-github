c3a42d02786c68ee773098e24e251674db696f6c
rahim <rahim.nagori@gmail.com>
2020-10-18 16:02:31 +0530

    project sangam design

---
+++ index.php
@@ -0,0 +1,228 @@
+<?php include 'include/header.php'; ?>
+<link rel="stylesheet" type="text/css" href="css/lightgallery.css">
+<div class="top_scrider">
+  <div class="owl-carousel owl-theme slider_arrrw" id="slider_galler5">
+
+    <div class="item">
+<div class="banner3" style="background: url(img/img_1.png);">
+  
+</div>
+    </div>
+
+    <div class="item">
+<div class="banner3" style="background: url(img/img_2.png);">
+
+</div>
+    </div>
+
+    <div class="item">
+<div class="banner3" style="background: url(img/img_3.png);">
+
+</div>
+    </div>
+
+    <div class="item">
+<div class="banner3" style="background: url(img/img_4.png);">
+
+</div>
+    </div>
+
+
+  </div>
+</div>
+
+<div class="sec_pad aboout">
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-6">
+        <div class="hous_box1">
+          <img src="img/img_5.png" class="img_r">
+        </div>
+      </div>
+      <div class="col-sm-6">
+        <div class="hous_box2">
+          <div class="headding">
+        <h1>About Us</h1>
+      </div>
+      <p>
+         Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.   Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+      </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="sec_pad  Ouer_servidee">
+    <div class="container">
+       <div class="headding">
+        <h1>OUR Services</h1>
+        <p>
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+          <br> industry's standard dummy text ever since Lorem Ipsum.
+        </p>
+      </div>
+      <div id="lightgallery" class="list-unstyled row">
+       <div class="col-sm-4" data-src="img/img_1.png">
+            <div class="hous_box3">
+              <a href="">
+                <img class="img_r" src="img/img_1.png">
+            </a>
+            </div>
+        </div>
+       <div class="col-sm-4" data-src="img/img_2.png">
+            <div class="hous_box3">
+              <a href="">
+                <img class="img_r" src="img/img_2.png">
+            </a>
+            </div>
+        </div>
+       <div class="col-sm-4" data-src="img/img_3.png">
+            <div class="hous_box3">
+              <a href="">
+                <img class="img_r" src="img/img_3.png">
+            </a>
+            </div>
+        </div>
+       <div class="col-sm-4" data-src="img/img_4.png">
+            <div class="hous_box3">
+              <a href="">
+                <img class="img_r" src="img/img_4.png">
+            </a>
+            </div>
+        </div>
+       <div class="col-sm-4" data-src="img/img_5.png">
+            <div class="hous_box3">
+              <a href="">
+                <img class="img_r" src="img/img_5.png">
+            </a>
+            </div>
+        </div>
+       <div class="col-sm-4" data-src="img/img_6.png">
+            <div class="hous_box3">
+              <a href="">
+                <img class="img_r" src="img/img_6.png">
+            </a>
+            </div>
+        </div>
+      </div>
+    
+    </div>
+</div>
+<div class="sec_pad  ouser_dma">
+  <div class="container">
+    <!-- <div class="headding">
+        <h1>OUR Interesting Facts</h1>
+        <p>
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+          <br> industry's standard dummy text ever since Lorem Ipsum.
+        </p>
+      </div> -->
+    <div class="row">
+      <div class="col-sm-3">
+        <div class="hous_box4">
+            <img src="img/img_8.png">   
+          <h1 class="count">35000</h1>
+          <h4>Total Projects</h4>
+        </div>
+      </div>
+      <div class="col-sm-3">
+        <div class="hous_box4">
+            <img src="img/img_9.png">   
+          <h1 class="count">700</h1>
+          <h4>3D Township Projects</h4>
+        </div>
+      </div>
+      <div class="col-sm-3">
+        <div class="hous_box4">
+            <img src="img/img_10.png">   
+          <h1 class="count">12000</h1>
+          <h4>3D Bungalow Projects</h4>
+        </div>
+      </div>
+      <div class="col-sm-3">
+        <div class="hous_box4">
+            <img src="img/img_11.png">   
+          <h1 class="count">198000</h1>
+          <h4>People Like</h4>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="sec_pad cliserff">
+  <div class="container">
+    <div class="headding">
+        <h1>OUR Testimonial</h1>
+        <p>
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+          <br> industry's standard dummy text ever since Lorem Ipsum.
+        </p>
+      </div>
+      <div class="sliderr_cllee">
+        <div class="owl-carousel owl-theme slider_arrrw" id="Testimonial">
+
+        <div class="item">
+          <div class="hous_box6">
+          <div class="hous_box5">
+            <div class="testimonial-content">
+              <div class="testimonial-icon">
+                  <i class="fa fa-quote-left"></i>
+              </div>
+              <p class="description">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent bibendum dolor sit amet eros imperdiet, sit amet hendrerit nisi vehicula.
+              </p>
+          </div>
+          <div class="hous_box8">
+            <img src="img/img_12.png" class="hous_box7">
+          <h3 class="title">williamson</h3>
+          <span class="post">Design Scope</span>
+        </div>
+          </div>
+          </div>
+        </div>
+        <div class="item">
+          <div class="hous_box6">
+          <div class="hous_box5">
+            <div class="testimonial-content">
+              <div class="testimonial-icon">
+                  <i class="fa fa-quote-left"></i>
+              </div>
+              <p class="description">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent bibendum dolor sit amet eros imperdiet, sit amet hendrerit nisi vehicula.
+              </p>
+          </div>
+          <div class="hous_box8">
+            <img src="img/img_12.png" class="hous_box7">
+          <h3 class="title">williamson</h3>
+          <span class="post">Pride Group</span>
+          </div>
+          </div>
+          </div>
+        </div>
+
+      </div>
+      </div>
+  </div>
+</div>
+<?php include 'include/footer.php'; ?>
+<script type="text/javascript" src="js/lightgallery-all.min.js"></script>
+<script type="text/javascript">
+  $(document).ready(function(){
+    $('#lightgallery').lightGallery();
+  });
+</script>
+
+<script type="text/javascript">
+  $('.count').each(function () {
+  $(this).prop('Counter',0).animate({
+  Counter: $(this).text()
+  }, {
+  duration: 4000,
+  easing: 'swing',
+  step: function (now) {
+  $(this).text(Math.ceil(now));
+  }
+  });
+  });
+</script>
